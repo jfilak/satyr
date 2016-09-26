52c60bda2eb672893d130144266a9d610910483b
Jakub Filak <jfilak@redhat.com>
2016-09-26 15:14:34 +0200

    add python bindings
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- tests/python/javascript.py
+++ tests/python/javascript.py
@@ -3,10 +3,10 @@
 import unittest
 from test_helpers import *
 
-path = '../nodejs_stacktraces/node-01'
+path = '../js_stacktraces/node-01'
 contents = load_input_contents(path)
 frames_expected = 10
-expected_short_text = ''
+expected_short_text = '#1 at Object.<anonymous> (/tmp/index.js:2:1)\n#2 at Module._compile (module.js:556:32)\n#3 at Object.Module._extensions..js (module.js:565:10)\n#4 at Module.load (module.js:473:32)\n#5 at tryModuleLoad (module.js:432:12)\n#6 at Function.Module._load (module.js:424:3)\n'
 
 class TestJavaScriptStacktrace(BindingsTestCase):
     def setUp(self):
@@ -42,13 +42,17 @@ class TestJavaScriptStacktrace(BindingsTestCase):
         self.assertEqual(self.trace.to_short_text(6), expected_short_text)
 
     def test_bthash(self):
-        self.assertEqual(self.trace.get_bthash(), '6124e03542a0381b14ebaf2c5b5e9f467ebba33b')
+        self.assertEqual(self.trace.get_bthash(), 'f4b36e13fd620fc6c650241b5753f4629397c7d6')
 
     def test_duphash(self):
-        expected_plain = ''
+        expected_plain = '''Thread
+/tmp/index.js:2
+module.js:556
+module.js:565
+'''
 
         self.assertEqual(self.trace.get_duphash(flags=satyr.DUPHASH_NOHASH, frames=3), expected_plain)
-        self.assertEqual(self.trace.get_duphash(), '')
+        self.assertEqual(self.trace.get_duphash(), 'e3c6aae82e127c2748a36fc3eec7c5eb41493fd3')
 
     def test_crash_thread(self):
         self.assertTrue(self.trace.crash_thread is self.trace)
@@ -62,13 +66,13 @@ class TestJavaScriptStacktrace(BindingsTestCase):
             "exception_name": "ReferenceError",
             "stacktrace": [
                 {
-                    "file_name": "/usr/lib/npm/modules/foo/bar.js",
+                    "file_name": "bar.js",
                     "function_name": "do_nothing",
                     "file_line": 42,
                     "line_column": 24,
                 },
                 {
-                    "file_name": "/usr/lib/npm/modules/bar/foo.js",
+                    "file_name": "foo.js",
                     "function_name": "do_everything",
                     "file_line": 21,
                     "line_column": 12,
@@ -78,17 +82,17 @@ class TestJavaScriptStacktrace(BindingsTestCase):
 '''
         trace = satyr.JavaScriptStacktrace.from_json(json_text)
         self.assertEqual(trace.exception_name, 'ReferenceError')
-        self.assertEqual(len(trace.frames), 1)
+        self.assertEqual(len(trace.frames), 2)
 
-        self.assertEqual(trace.frames[0].file_name, "/usr/lib/npm/modules/foo/bar.js")
+        self.assertEqual(trace.frames[0].file_name, "bar.js")
         self.assertEqual(trace.frames[0].function_name, 'do_nothing')
         self.assertEqual(trace.frames[0].file_line, 42)
         self.assertEqual(trace.frames[0].line_column, 24)
 
-        self.assertEqual(trace.frames[0].file_name, "/usr/lib/npm/modules/bar/foo.js")
-        self.assertEqual(trace.frames[0].function_name, 'do_everything')
-        self.assertEqual(trace.frames[0].file_line, 21)
-        self.assertEqual(trace.frames[0].line_column, 12)
+        self.assertEqual(trace.frames[1].file_name, "foo.js")
+        self.assertEqual(trace.frames[1].function_name, 'do_everything')
+        self.assertEqual(trace.frames[1].file_line, 21)
+        self.assertEqual(trace.frames[1].line_column, 12)
 
     def test_hash(self):
         self.assertHashable(self.trace)
@@ -100,7 +104,7 @@ class TestJavaScriptFrame(BindingsTestCase):
 
     def test_str(self):
         out = str(self.frame)
-        self.assertEqual(out, "at Object.<anonymous> (/home/jfilak/repos/abrt-nodejs/testapp/index.js:2:1)")
+        self.assertEqual(out, "at Module._compile (module.js:556:32)")
 
     def test_dup(self):
         dup = self.frame.dup()
@@ -120,10 +124,10 @@ class TestJavaScriptFrame(BindingsTestCase):
         self.assertFalse(dup.equals(self.frame))
 
     def test_getset(self):
-        self.assertGetSetCorrect(self.frame, 'file_name', '/usr/lib/npm/modules/foo/bar.js', '/usr/lib/npm/modules/bar/foo.js')
-        self.assertGetSetCorrect(self.frame, 'file_line', 10, 6667)
-        self.assertGetSetCorrect(self.frame, 'line_column', 10, 6669)
-        self.assertGetSetCorrect(self.frame, 'function_name', 'Object.<anynomous>', 'iiiiii')
+        self.assertGetSetCorrect(self.frame, 'file_name', 'module.js', '/usr/lib/npm/modules/bar/foo.js')
+        self.assertGetSetCorrect(self.frame, 'file_line', 556, 6667)
+        self.assertGetSetCorrect(self.frame, 'line_column', 32, 6669)
+        self.assertGetSetCorrect(self.frame, 'function_name', 'Module._compile', 'iiiiii')
 
     def test_hash(self):
         self.assertHashable(self.frame)
